a0a9bde91715d0da89b6af34ea7de9e8c1496933
DarrelWirasena <benedictusdarrel9@gmail.com>
2025-10-17 16:02:42 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,43 @@
+login:
+
+<?php
+session_start();
+$username_valid = "unnes";
+$password_valid = "12345";
+
+
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    header("Location: index.html");
+    exit;
+}
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+
+    if (($username === $username_valid) && ($password === $password_valid)) {
+        
+        $_SESSION['login'] [] = [
+            "username" => $username,
+            "password" => $password,
+            "login_at" => date("Y-m-d H:i:s")
+        ];
+
+        // jika benar
+        echo "selamat datang : " . $username . " anda teah login sebanyak: " . count($_SESSION['login']) . " kali";
+
+        echo '<br>';
+
+        echo '<a href="index.html">Back to login</a>';
+
+        echo '<br>';
+
+        echo '<a href="logout.php">Logout</a>';
+
+        echo '<pre>';
+        var_dump($_SESSION['login']);
+    }else{
+        // jika salah
+        echo "username atau password salah";
+    }
